040cd1d7d354228adf7e43fe7d452b4fb325a9ce
Evgenia0707 <4030071@gmail.com>
2022-08-30 16:02:27 -0400

    interface segregation principle good example

---
+++ src/main/java/com/company/solid/interfaceSegregation/example/good/Owl.java
@@ -0,0 +1,18 @@
+package com.company.solid.interfaceSegregation.example.good;
+
+public class Owl implements CanEat,CasnWalk, CanFly{
+    @Override
+    public void eat() {
+        System.out.println("Eating.");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flying.");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Walking.");
+    }
+}
